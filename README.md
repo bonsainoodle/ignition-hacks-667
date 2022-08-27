d27883d9d0a885619a63c1ff00435ad594bc2a90
Tom Jurien <3sprIx.exe@gmail.com>
2022-08-27 18:13:43 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+todo :
+add deepl translation api
+add speaker module
+find how to speak though a new virtual microphone and not exisiting one
--- stream.py
+++ stream.py
@@ -5,53 +5,21 @@ import symbl
 app_id = "7433624d4367486372624573506a7654566b726434716747554f546b66486546"
 app_secret = "48414d786f4c2d796936397768304574506630682d4a644e4568376e62443539534f425a564e53585838524c495a6657384e78633739306b7954542d725f456e"
 
-config = {
-  "meetingTitle": "appel slovaquie",
-  
-}
 
-speaker = {
-  "name": "grigor",
-  "email": "vladimirjurien@gmail.com"
-}
+config = {"confidenceThreshold": 0.5, "speechRecognition": {"encoding": "LINEAR16", "sampleRateHertz": 48000}}
 
-# STOP changing values here.
 
 events = {
     "message_response": lambda response: print(
         "Final Messages -> ",
         [message["payload"]["content"] for message in response["messages"]],
-    ),
-    "message": lambda response: print(
-        "live transcription : {}".format(
-            response["message"]["punctuated"]["transcript"]
-        )
     )
-    if "punctuated" in response["message"]
-    else print(response),
-    "insight_response": lambda response: [
-        print(
-            "Insights Item of type {} detected -> {}".format(
-                insight["type"], insight["payload"]["content"]
-            )
-        )
-        for insight in response["insights"]
-    ],
-    "topic_response": lambda response: [
-        print(
-            "Topic detected -> {} with root words, {}".format(
-                topic["phrases"], topic["rootWords"]
-            )
-        )
-        for topic in response["topics"]
-    ],
 }
 
 connection_object = symbl.Streaming.start_connection(
     credentials={"app_id": app_id, "app_secret": app_secret},
     insight_types=["question", "action_item", "follow_up", "topic"],
-    speaker=speaker,
-    config=config
+    config=config,
 )
 
 connection_object.subscribe(events)
